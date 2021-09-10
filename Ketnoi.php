967b270b262ba4f443f504fd9d66ccf35fa4412b
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-10 15:48:01 +0700

    Create Ketnoi.php

---
+++ Ketnoi.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "123456";
+$database = "Webbanhang";
+
+//create connection
+
+$conn = new mysqli($servername,$username,$password,$database );
+// check connect
+if( $conn->connect_error)
+  die("Loi hien thi: ".$conn->connect_error);
+
+
+ ?>
