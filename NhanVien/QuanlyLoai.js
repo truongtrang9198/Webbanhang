7ebfc2d3cef75a6335d195736060db51874e58e2
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-18 00:14:43 +0700

    Create QuanlyLoai.js

---
+++ NhanVien/QuanlyLoai.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('#Them').click(function(){
+    var Tenloai = $('#Tenloai').val().trim();
+    $.get("Xulythemloai.php",{Tenloai:Tenloai},function(data){
+      alert(data);
+      location.reload();
+    });
+  })
+// sua loai phong
+$('#Sualoai').click(function(){
+    var Maloai = $('#Sualoai').val().trim();
+    var Tenloai = prompt("Nhập vào tên loại hàng mới:");
+
+    if(Tenloai!=''){
+      $.get("Xulysualoai.php",{Maloai:Maloai,Tenloai:Tenloai},function(data){
+            alert(data);
+            location.reload();
+      })
+    }else{
+      alert("Bạn chưa nhập dữ liệu!");
+    }
+});
+
+})
