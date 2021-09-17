7d2836c5ec7090aba8166ec17f3ec02b90d2a2c6
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-18 00:15:11 +0700

    Create XulyxoaHH.php

---
+++ Quanly/XulyxoaHH.php
@@ -0,0 +1,17 @@
+<?php
+  include("Ketnoi.php");
+  if(isset($_POST['mahang'])){
+    $mahang = $_POST['mahang'];
+      $sql = "delete from HangHoa where MSHH='$mahang'";
+      if(mysqli_query($conn,$sql)){
+        echo "Thành công!";
+      }else{
+        echo "Thất bại!";
+      }
+  }else {
+    echo "khong bat duoc du lieu";
+  }
+
+
+
+ ?>
