175d8140fec85aff61a95b8f1a196aa416630337
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-18 00:16:12 +0700

    Create Taianhlen.php

---
+++ NhanVien/Taianhlen.php
@@ -0,0 +1,16 @@
+<?php
+    include("../Ketnoi.php");
+      $mahang = $_POST['mahang'];
+      $hinhanh = $_FILES['hinhanh']['name'];
+      $tmp_name = $_FILES['hinhanh']['tmp_name'];
+      $path="../HinhAnh/";
+      move_uploaded_file($tmp_name,$path.$hinhanh);
+      $img_url = $path.$hinhanh;
+      $sql = "insert into HinhHangHoa(MSHH,TenHinh) values('$mahang','$img_url')";
+      if(mysqli_query($conn,$sql)){
+        echo("Tải ảnh thành công");
+      }else{
+        echo "Tải ảnh thất bại!";
+      }
+
+ ?>
