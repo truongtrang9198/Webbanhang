811e82df961009ccc0b6025bf6d61faf782db4ef
truongtrang9198 <truongtrang9198@gmail.com>
2021-09-18 00:14:46 +0700

    Update QuanlyLoai.php

--- NhanVien/QuanlyLoai.php
+++ NhanVien/QuanlyLoai.php
@@ -10,15 +10,35 @@
        .btn{
          background-color: #7A8B8B;
        }
+       #form-search >input{
+         width: 300px !important;
+         border-bottom: 1px solid;
+         border-color: #7A8B8B;
+         margin-left: 50px;
+       }
+
      </style>
+     <script src="QuanlyLoai.js"></script>
    </head>
    <body>
  <div class="container-fluid">
 <h3>QUẢN LÝ LOẠI HÀNG HÓA</h3>
-<form class="form-inline" action="#" method="get">
-        <input type="text" class="form-control mr-sm-2" id="Timkiem" name="" value="" placeholder="Search ...">
-        <button type="submit" class="btn" name="button"><i class="fas fa-search"></i> </button>
-</form>
+<div class="row">
+  <div class="col-md-5">
+    <form class="form-inline"   method="post">
+      <input type="text" name="" value="" class="form-control" placeholder="Nhập tên loại hàng" id="Tenloai">
+      <button type="button" name="button" class="btn" id="Them">Thêm</button>
+    </form>
+  </div>
+  <div class="col-md-4 offset-3">
+    <form class="form-inline" id="form-search" action="#" method="get">
+            <input type="text" class="form-control" id="Timkiem" name="" value="" placeholder="Search ...">
+            <button type="submit" class="btn" name="button"><i class="fas fa-search"></i> </button>
+    </form>
+  </div>
+</div>
+
+
 <table class="table table-striped table-hover" id="danhsachloai">
     <thead>
       <tr>
@@ -26,44 +46,28 @@
         <th>Mã loại</th>
         <th>Tên Loại</th>
         <th> </th>
-        <th></th>
+
       </tr>
     </thead>
     <tbody>
-      <tr>
         <?php
         $sql ="select * from LoaiHangHoa LIMIT 6";
         $query = mysqli_query($conn,$sql);
         $n = 0;
         while($row =mysqli_fetch_array($query)){
          ?>
+         <tr>
         <td><?php echo $n; ?></td>
         <td><?php echo $row['MaLoaiHang']; ?></td>
         <td><?php echo $row['TenLoaiHang']; ?></td>
-        <td> <a href="#"> <i class="fas fa-edit"></i></a> </td>
-        <td><a href="#"><i class="fas fa-trash"></i></a> </td>
+        <td> <button class="btn" value=" <?php echo $row['MaLoaiHang']; ?>" id="Sualoai">
+           <i class="fas fa-edit"></i></button> </td>
         <?php $n++;
       }
     ?>
       </tr>
     </tbody>
 </table>
-<form class="form-inline" action="index.html" method="post">
-  <input type="text" name="" value="" class="form-control" placeholder="Nhập tên loại hàng" id="Tenloai">
-  <button type="button" name="button" class="btn" id="Them">Thêm</button>
-</form>
  </div>
 </body>
 </html>
-<script type="text/javascript">
-  $.document.ready(function(){
-    $('#Them').click(function(){
-      var Tenloai = $('#Tenloai').val();
-      $.get("Xulythemloai.php",{Tenloai:Tenloai},function(data){
-        alert(data);
-      })
-    })
-
-
-  })
-</script>
