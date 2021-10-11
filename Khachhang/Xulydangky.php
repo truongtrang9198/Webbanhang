ab9e41a835913f172a2bc3ff8ab4849004eac3d1
truongtrang9198 <truongtrang9198@gmail.com>
2021-10-11 23:05:21 +0700

    Create Xulydangky.php

---
+++ Khachhang/Xulydangky.php
@@ -0,0 +1,30 @@
+<?php
+include_once 'Ketnoi.php';
+
+$hoten = $_POST['hoten'];
+$tencty = $_POST['tencty'];
+$sdt = $_POST['sdt'];
+$sofax =$_POST['sofax'] ;
+$matkhau = $_POST['matkhau'];
+$matkhau = md5($matkhau);
+//  kiem tra sdt co ton tai chua
+$sql = "select SoDienThoai from KhachHang";
+$query = mysqli_query($conn,$sql);
+if(mysqli_num_rows($query) != 0){
+//    $row = mysqli_fetch_array($query);
+  while ($row = mysqli_fetch_array($query)) {
+    if($row['SoDienThoai'] == $sdt){
+      echo("Số điện thoại đã được đăng ký!");
+      die();
+    }
+  }
+}
+
+$sql = "insert into khachHang(HoTenKH,TenCongTy,SoDienThoai,SoFax,Passwd)
+          values('$hoten','$tencty','$sdt','$sofax','$matkhau')";
+  if(mysqli_query($conn,$sql)){
+    echo("Thành công!");
+  }else{
+    echo("Thất bại!");
+  }
+ ?>
