7ca0782a6787bcb834b225a5dd5de2e8c176019c
truongtrang9198 <truongtrang9198@gmail.com>
2021-10-11 23:04:57 +0700

    Create Footer.php

---
+++ Khachhang/Footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>Mọi thắc mắc xin vui lòng liên hệ: </p>
+  <p><i class="fas fa-home"></i> Khu II, đường 3/2, P. Xuân Khánh, Q. Ninh Kiều, TP. Cần Thơ</p>
+  <p><i class="fas fa-mobile-alt"></i> 0949760xxx</p>
+  <p><i class="fas fa-envelope"></i> Totoshop@gmail.com</p>
+</footer>
