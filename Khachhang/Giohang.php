b41f50ea90a9a3beb4a67012d0cc12ef5ceb52b1
truongtrang9198 <truongtrang9198@gmail.com>
2021-10-11 23:05:00 +0700

    Create Giohang.php

---
+++ Khachhang/Giohang.php
@@ -0,0 +1,11 @@
+<?php
+  if(isset($_POST['id'])){
+    $mahang = $_POST['id'];
+    echo "mã hàng:".$mahang;
+  }
+    // if(isset($_SESSION['MSKH'])){
+    //   $mahang = $_SESSION['MSKH'];
+    //   echo $mahang;
+    // }
+
+ ?>
