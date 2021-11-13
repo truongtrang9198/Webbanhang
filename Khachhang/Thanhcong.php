0e705f620003e698250146467d3c9df8ffa7bb66
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-14 01:59:43 +0700

    Create Thanhcong.php

---
+++ Khachhang/Thanhcong.php
@@ -0,0 +1,8 @@
+
+  <div style="text-align: center">
+      <img src="QZJI.gif" alt="">
+      <h1>Đặt hàng thành công!</h1>
+      <h4>Đơn hàng sẽ giao tới bạn sớm!</h4>
+      <h4>Cám ơn bạn đã mua hàng!</h4>
+      <a href="index.php">Trở về trang chủ</a>
+  </div>
