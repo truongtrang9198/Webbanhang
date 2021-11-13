74dbfbe65ea4ded2464fb6167d8103e7fe88514c
zhang <82966213+truongtrang9198@users.noreply.github.com>
2021-11-14 01:59:15 +0700

    Update Header_nav.js

--- Khachhang/Header_nav.js
+++ Khachhang/Header_nav.js
@@ -1,5 +1,7 @@
 
   $(document).ready(function(){
+// javascript phần đăng ký và đăng nhập hệ thống --------------------------------------------------------------
+// Đăng nhập vào hệ thống
     $('#form_dn').submit(function(e){
         e.preventDefault();
         var sdt = $('#sdt').val();
@@ -17,7 +19,28 @@
         });
     });
 
+// Đăng ký vào hệ thống
+$('#form_dangky').submit(function(e){
+  e.preventDefault();
+  var hoten = $.trim($('#hoten').val());
+  var tencty = $.trim($('#tencty').val());
+  var sdt = $.trim($('#sdt_dk').val());
+  var sofax = $.trim($('#sofax').val());
+  var matkhau = $.trim($('#matkhau_dk').val());
+  var xc_matkhau = $.trim($('#xc_matkhau').val());
+  if(matkhau != xc_matkhau){
+    $('#matkhau_dk').css('border-color','red');
+    $('#xc_matkhau').css('border-color','red');
+  }else{
+    $.post("Xulydangky.php",{hoten:hoten,tencty,tencty,sdt:sdt,sofax:sofax,matkhau:matkhau},
+                          function(thongbao){
+        alert(thongbao);
+        if(thongbao == "Thành công!"){
+          location.href = "index.php?action=trangchu";
+        }
+      });
+  }
+});
 
-
-
+ 
   });
